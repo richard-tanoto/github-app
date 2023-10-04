48ef83ab06a5ba0b0952c0ab14599fe42a404d03
Richard Tanoto <richard.tanoto@bni.co.id>
2023-10-04 10:53:10 +0700

    Add searchbar layout

--- app/src/main/java/com/richard/githubapp/HomeFragment.kt
+++ app/src/main/java/com/richard/githubapp/HomeFragment.kt
@@ -2,15 +2,9 @@ package com.richard.githubapp
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.richard.githubapp.databinding.FragmentHomeBinding
 
@@ -32,35 +26,26 @@ class HomeFragment : Fragment() {
         setupToolbar()
         setupOnClick()
         //TODO 1: Show user list
+        //TODO 5: Implement search feature
     }
 
     private fun setupToolbar() {
-        (requireActivity() as AppCompatActivity).apply {
-            setSupportActionBar(binding.toolbar)
-            supportActionBar?.title = getString(R.string.app_name)
-            supportActionBar?.setDisplayHomeAsUpEnabled(false)
-        }
-        requireActivity().addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.home_toolbar_menu, menu)
-            }
-
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                return when (menuItem.itemId) {
+        binding.searchBar.apply {
+            inflateMenu(R.menu.home_toolbar_menu)
+            setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
                     R.id.destFavorite -> {
                         findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToFavoriteFragment())
                         true
                     }
-
                     R.id.destSetting -> {
                         findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToSettingFragment())
                         true
                     }
-
                     else -> false
                 }
             }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        }
     }
 
     private fun setupOnClick() {
